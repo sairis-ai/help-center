6ee9bdb58d2b1ca107f38671b0a5905251d264d3
Robert Smith <rsmith@sairis.ai>
2026-03-12 14:58:27 -0600

    chore: Enable trigger on push for CodeStar connection in PipelineStack

--- cdk/lib/pipeline-stack.ts
+++ cdk/lib/pipeline-stack.ts
@@ -162,20 +162,12 @@ export class PipelineStack extends cdk.Stack {
       connectionArn: 'arn:aws:codeconnections:us-west-2:884568634535:connection/263e2322-42f4-49e9-9b72-ef8f1c919e5b',
       branch: 'main',
       output: outputSources,
-      triggerOnPush: false,
+      triggerOnPush: true,
     })
 
     pipeline.addStage({
       stageName: 'Source',
       actions: [sourceAction],
-    })
-
-    pipeline.addTrigger({
-      providerType: codepipeline.ProviderType.CODE_STAR_SOURCE_CONNECTION,
-      gitConfiguration: {
-        sourceAction,
-        pushFilter: [{ branchesIncludes: ['main'] }],
-      },
     })
 
     const invalidateHelpCenter = new codebuild.PipelineProject(this, `InvalidateHelpCenter`, {
